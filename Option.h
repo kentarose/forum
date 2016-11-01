fa190a4cc39ec0fceff19f6e1e9d1dffd009432b
kentarose <ertt27@naver.com>
2016-11-01 20:42:38 +0900

    real menu
    메뉴 화면

--- Option.h
+++ Option.h
@@ -9,12 +9,32 @@ class Option : public cocos2d::LayerColor
 
 public:
 	virtual bool init();
-    static cocos2d::Scene* createScene();
+	static cocos2d::Scene* createScene();
 	void menuCloseCallback(Ref* pSender);
-    // implement the "static create()" method manually
-    CREATE_FUNC(Option);
+	// implement the "static create()" method manually
+	CREATE_FUNC(Option);
+
+	bool onTouchBegan(Touch* touch, Event* unsued_event);
+	void onTouchMoved(Touch* touch, Event* unsued_event);
+
+	Layer *SLayer;
+
+	Sprite *backsound_on;
+	Sprite *backsound_off;
+	Sprite *effectsound_on;
+	Sprite *effectsound_off;
+
+	bool isbgmoff;
+	bool isegmoff;
+	float TouchPosx;
+	float TouchPosy;
 
 	void Go_Option(Ref *sender);
+	void Option_on(Ref *sender);
+	void Option_off(Ref* pSender);
+
+	bool isoption;
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif // __OPTION_H__
+
